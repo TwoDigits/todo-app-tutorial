31d37c33708684f4f1c0c6a92dcb92ba5d60fa50
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2018-11-24 11:04:20 +0100

    Install dependencies before deploying app

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,18 @@ OUTPUT_FILE=sam-template-output.yaml
 STAGE_NAME=dev
 STACK_NAME=todo-app-$STAGE_NAME
 
+basePath=$(pwd)
+
+cd $basePath/src
+for d in *; do
+    if [[ -d $d ]]; then
+        cd "$d"
+        echo "Building $d..."
+        npm install --silent
+        cd ..
+    fi
+done
+cd $basePath
+
 aws cloudformation package --template-file $INPUT_FILE --output-template-file $OUTPUT_FILE --s3-bucket $S3_BUCKET
 aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --parameter-overrides StageName=$STAGE_NAME --capabilities CAPABILITY_IAM
