d18c0103e5a8419a20a6e6d959ef782572becf41
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2018-11-24 10:00:29 +0100

    Read tasks from a DynamoDB table

--- src/list-tasks/index.js
+++ src/list-tasks/index.js
@@ -1,3 +1,7 @@
+const aws = require('aws-sdk');
+
+const TasksTableName = process.env.TABLE_NAME;
+
 exports.handler = async (event) => {
   const tasks = await listTasks();
 
@@ -11,18 +15,16 @@ exports.handler = async (event) => {
 };
 
 async function listTasks() {
-  return [
-    {
-      id: '22d157f8-5812-457e-8fbd-db49aaa25c45',
-      title: 'Some Todo',
-      description: 'Lorem ipsum dolor est',
-      done: false
-    },
-    {
-      id: '22d157f8-5812-457e-8fbd-db49aaa25c46',
-      title: 'Another Todo',
-      description: 'Lorem ipsum dolor est',
-      done: true
-    }
-  ];
+  const params = {
+    TableName: TasksTableName
+  };
+
+  const dynamoDB = new aws.DynamoDB.DocumentClient();
+  try {
+    const results = await dynamoDB.scan(params).promise();
+    return results.Items;
+  } catch (error) {
+    console.log('Failed to fetch tasks: ' +  error);
+    throw error;
+  }
 }
