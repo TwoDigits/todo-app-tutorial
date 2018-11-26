32d608c8304faef344b9e2272e377c1d526d96ac
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2018-11-26 10:49:28 +0100

    URL of API Gateway is printed

--- deploy.sh
+++ deploy.sh
@@ -21,3 +21,6 @@ cd $basePath
 
 aws cloudformation package --template-file $INPUT_FILE --output-template-file $OUTPUT_FILE --s3-bucket $S3_BUCKET
 aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --parameter-overrides StageName=$STAGE_NAME --capabilities CAPABILITY_IAM
+
+API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[0].OutputValue')
+echo "API Gateway URL: $API_GATEWAY_URL"
