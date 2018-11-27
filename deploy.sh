18238071804fd2c64694aca0a90171aeefc324cd
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2018-11-27 10:17:56 +0100

    Use explicit IAM role for deploying stack

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ done
 cd $basePath
 
 aws cloudformation package --template-file $INPUT_FILE --output-template-file $OUTPUT_FILE --s3-bucket $S3_BUCKET
-aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --parameter-overrides StageName=$STAGE_NAME --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --parameter-overrides StageName=$STAGE_NAME --role-arn arn:aws:iam::022876999554:role/CloudFormationDeploymentRole --capabilities CAPABILITY_IAM
 
 API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[0].OutputValue')
 echo "API Gateway URL: $API_GATEWAY_URL"
